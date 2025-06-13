ba74852b42724b1907dd092d2873d8266962bbdd
fahad-6sensehq <fahad@6sensehq.com>
2025-06-13 12:14:03 +0600

    removed unused dependencies

--- src/app.configuration.ts
+++
@@ -1,7 +0,0 @@
-export default () => ({
-    HOST: process.env.HOST,
-    PORT: process.env.PORT,
-    USERNAME: process.env.USERNAME,
-    PASSWORD: process.env.PASSWORD,
-    DATABASE: process.env.DATABASE,
-});
